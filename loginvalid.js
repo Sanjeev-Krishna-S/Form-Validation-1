0203a96b6156a756e080f2f55aee1f988953f05a
Sanjeev Krishna S <sanjeevkrishna2001x@gmail.com>
2023-10-24 16:49:26 +0530

    login changes

--- loginvalid.js
+++ loginvalid.js
@@ -1,5 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
     const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
+    const loginButton = document.querySelector("#login-form button[type='submit']");
 
     // Email Validation
     emailInput.addEventListener("input", function() {
@@ -12,6 +14,16 @@ document.addEventListener("DOMContentLoaded", function() {
             emailInput.classList.remove("is-invalid");
         } else {
             emailInput.classList.add("is-invalid");
+        }
+    });
+
+    // Form submission validation
+    loginButton.addEventListener("click", function(event) {
+        if (passwordInput.value.trim() === "") {
+            event.preventDefault(); // Prevent the form from being submitted
+            document.querySelector(".invalid-feedback-password").style.display = "block"; // Show the error message
+        } else {
+            document.querySelector(".invalid-feedback-password").style.display = "none"; // Hide the error message if the password is entered
         }
     });
 });
